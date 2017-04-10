66e2a5c2c8f2236cc6ec1eb81649beca7699cba5
Sergio Martins <spm5065@rit.edu>
2017-04-10 18:52:21 -0400

    Added more random functions

--- Randoms.c
+++ Randoms.c
@@ -13,7 +13,18 @@ unsigned char RandomNextCustomerTime(){
 	return ((char) rand() % 4 + 1);
 }
 
-//Get time to service a customer in increments of 30seconds
+//Get time to service a customer in increments of 30seconds ( 30 seconds to 8 minutes )
 unsigned short RandomTransactionTime(){
 	return ((short) rand() % 16 + 1 );
 }
+
+//Get length of next break ( 1 to 4 min)
+unsigned short RandomBreakLength(){
+	return ((short) rand() % 4 + 1);
+}
+
+//Get time of next break in minutes (30 min to 60 min)
+unsigned short RandomBreakTime(){
+	return((short) rand() % 31 + 30);
+}
+
--- Randoms.h
+++ Randoms.h
@@ -1,4 +1,15 @@
 #pragma once
 
-unsigned char RandomNextCustomerTime(); //Get time for next customer to appear in queue in minutes
-unsigned short RandomTransactionTime();  //Get time to service a customer in increments of 30seconds
+unsigned char RandomNextCustomerTime();	//Get time for next customer to appear in queue in minutes
+unsigned short RandomTransactionTime();	//Get time to service a customer in increments of 30seconds
+unsigned short RandomBreakLength();		//Get length of next break ( 1 to 4 min)
+unsigned short RandomBreakTime();		//Get time of next break in minutes (30 min to 60 min)
+
+
+
+
+
+
+
+
+
