3cab184441045fefbdc97c62409d1e85df5b2d8e
smd6141 <smd6141@rit.edu>
2017-04-12 19:59:42 -0400

    Update metrics.c

--- metrics.c
+++ metrics.c
@@ -6,47 +6,47 @@
  */
 
 #include "bank.h"
-
+//time stored in 30 sec increments divide by 2 for minutes and cast to float for accuracy
 void calculate_metrics(){
 
 	//Total Customers Serviced
-	printf("Total Customers Serviced %lu/n " , Bank.customersServiced);
+	printf("Total Customers Serviced: %f/n " , (float)Bank.customersServiced);
 
 	//Average Queue Time
-	printf("Average Queue Time %lu/n " , Bank.totalTimeInQueue/customersServiced);
+	printf("Average Queue Time: %f minutes/n " , ((float)(Bank.totalTimeInQueue/customersServiced))/2 );
 
 	//Maximim Queue Time
-	printf("Maximim Queue Time %lu/n ", Bank.maxTimeInQueueCust );
+	printf("Maximim Queue Time: %f minutes/n ", ((float)Bank.maxTimeInQueueCust)/2 );
 
 	//Average Transaction Time
-	printf("Average Transaction Time %lu/n ", Bank.totalTimeWithTellers/customersServiced );
+	printf("Average Transaction Time: %f minutes/n ", ((float)(Bank.totalTimeWithTellers/customersServiced))/2 );
 
 	//Maximim Transaction Time
-	printf("Maximim Transaction Time %lu/n ", Bank.maxTimeInTransaction );
+	printf("Maximim Transaction Time: %f minutes/n ", ((float)Bank.maxTimeInTransaction)/2 );
 
 	//Average Teller Idle Time
-	printf("Average Teller Idle Time %lu/n ", Bank.totalTellerDowntime/3 );
+	printf("Average Teller Idle Time: %f minutes/n ", ((float)Bank.totalTellerDowntime/3)/2 );
 
 	//Longent Line
-	printf("Longent Line %lu/n ", Bank.maxNumInQueue );
+	printf("Longent Line: %f/n ", (float)Bank.maxNumInQueue );
 
 	//Total Breaks Per
-	printf("Teller One Total Breaks %lu/n ", tellers[0].numBreaks );
-	printf("Teller Two Total Breaks %lu/n ", tellers[1].numBreaks );
-	printf("Teller Three Total Breaks %lu/n ", tellers[2].numBreaks );
+	printf("Teller One Total Breaks: %f/n ", (float)tellers[0].numBreaks );
+	printf("Teller Two Total Breaks: %f/n ", (float)tellers[1].numBreaks );
+	printf("Teller Three Total Breaks: %f/n ", (float)tellers[2].numBreaks );
 
-	//Average breaks Per
-	printf("Teller One Average breaks %lu/n ", (tellers[0].totalBreak)/(tellers[0].numBreaks) );
-	printf("Teller Two Average breaks %lu/n ", (tellers[1].totalBreak)/(tellers[1].numBreaks) );
-	printf("Teller Three Average Breaks %lu/n ", (tellers[2].totalBreak)/(tellers[2].numBreaks) );
+	//Average break Per
+	printf("Teller One Average Break: %f minutes/n ", ((float)(tellers[0].totalBreak)/(tellers[0].numBreaks))/2 );
+	printf("Teller Two Average Break: %f minutes/n ", ((float)(tellers[1].totalBreak)/(tellers[1].numBreaks))/2 );
+	printf("Teller Three Average Break: %f minutes/n ", ((float)(tellers[2].totalBreak)/(tellers[2].numBreaks))/2 );
 
 	//Longest Break Per
-	printf("Teller One Longest Break %lu/n ", tellers[0].maxBreak );
-	printf("Teller Two Longest Break %lu/n ", tellers[1].maxBreak );
-	printf("Teller Three TLongest Break %lu/n ", tellers[2].maxBreak );
+	printf("Teller One Longest Break: %f minutes/n ", ((float)tellers[0].maxBreak)/2 );
+	printf("Teller Two Longest Break: %f minutes/n ", ((float)tellers[1].maxBreak)/2 );
+	printf("Teller Three TLongest Break: %f minutes/n ", ((float)tellers[2].maxBreak)/2 );
 
 	//Shortest Break Per
-	printf("Teller One Shortest Break %lu/n ", tellers[0].minBreak );
-	printf("Teller Two Shortest Break %lu/n ", tellers[1].minBreak );
-	printf("Teller Three Shortest Break %lu/n ", tellers[2].minBreak );
+	printf("Teller One Shortest Break: %f minutes/n ", ((float)tellers[0].minBreak)/2 );
+	printf("Teller Two Shortest Break: %f minutes/n ", ((float)tellers[1].minBreak)/2 );
+	printf("Teller Three Shortest Break: %f minutes/n ", ((float)tellers[2].minBreak)/2 );
 }
