55415d58c929aa02f34b68b4999d9787c11856aa
Sergio Martins <spm5065@rit.edu>
2017-04-20 21:17:58 -0400

    Completed and added an output screenshot

--- Bank.c
+++ Bank.c
@@ -11,23 +11,42 @@
 #include "teller.h"
 #include "customer.h"
 #include "queue.h"
+#include "metrics.h"
 
 struct BANK Bank = {0,0,0,0,0,0,0,0};
+
+pthread_t tellerThreads[3];
 
 int main(void) {
 	initQueue();
 	initTellers();
 
+	puts("The Bank is opening for the day..."); /* prints !!!Hello World!!! */
 
+	int turn = -1;
 
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	fflush(stdout);
 
-	doQueueAction();
+	//Start all the threads
+	for(int threadNum = 0; threadNum < 3; threadNum++)
+		if(pthread_create(&tellerThreads[threadNum], NULL, (void *) &runTeller, tellers[threadNum]))
+			puts("Couldn't create a thread!");
 
-	puts("!!!Goodbye Cruel World!!!");
+	//Then ensure the threads are doing stuff
+	while(doQueueAction());
+
+	turn = -1;
+
+	puts("The Bank has closed and finished the days business...");
+
+	for(int i = 0; i < 3; i++)
+		pthread_join(tellerThreads[i], NULL);
+
+	calculate_metrics();
 
 	destroyTellers();
 	destroyQueue();
+
 	return EXIT_SUCCESS;
 }
 
